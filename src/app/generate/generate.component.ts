e33f8ccea8e98cc58a08972eed06f4edcd720ff5
Ramachandran <ramachandrandvr@gmail.com>
2022-08-22 19:48:11 +0530

    splash file add project 1.0

---
+++ src/app/generate/generate.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-generate',
+  templateUrl: './generate.component.html',
+  styleUrls: ['./generate.component.css'],
+  encapsulation:ViewEncapsulation.None
+})
+export class GenerateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

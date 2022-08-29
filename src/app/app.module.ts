9ee5b137c92ef2e2777132f00314b19a653d4a96
Arunkumar <arunkumar.magudees@gmail.com>
2022-08-29 22:11:48 +0530

    arun file1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-<<<<<<< HEAD
 import{ HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,33 +8,26 @@ import { GenerateComponent } from './generate/generate.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SentotpComponent } from './sentotp/sentotp.component';
 
-=======
 
-import { AppComponent } from './app.component';
-import { GenerateComponent } from './generate/generate.component';
+
 import { OtpComponent } from './otp/otp.component';
 import { RouterModule } from '@angular/router';
->>>>>>> 5bed02f121835a3a89c0c016bf50210db6658de7
+
 
 @NgModule({
   declarations: [
     AppComponent,
     GenerateComponent,
-<<<<<<< HEAD
     DashboardComponent,
-    SentotpComponent
+    SentotpComponent,
+    OtpComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
-=======
-    OtpComponent
-  ],
-  imports: [
-    BrowserModule,
+
     
->>>>>>> 5bed02f121835a3a89c0c016bf50210db6658de7
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/demo.service.ts
+++ src/app/demo.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 
 @Injectable({
@@ -8,5 +9,5 @@ import { Injectable } from '@angular/core';
 
 export class DemoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 }

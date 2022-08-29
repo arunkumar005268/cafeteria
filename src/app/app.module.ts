5bed02f121835a3a89c0c016bf50210db6658de7
Ramachandran <ramachandrandvr@gmail.com>
2022-08-29 21:49:47 +0530

    ram file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { GenerateComponent } from './generate/generate.component';
+import { OtpComponent } from './otp/otp.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GenerateComponent
+    GenerateComponent,
+    OtpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
